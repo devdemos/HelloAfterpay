ec03f92dabc0fe7a60b2f3b57ff042204932ca40
James <sklirosd@gmail.com>
2019-11-12 22:26:17 +1100

    correct file path

--- config/config.bash
+++ config/config.bash
@@ -5,4 +5,4 @@ echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 sysctl -p
 
 export PATH=/opt/puppetlabs/bin:$PATH
-puppet apply --modulepath=/etc/puppetlabs/code/environments/production/modules /home/ec2-user/HelloAfterpay/site.pp
+puppet apply --modulepath=/etc/puppetlabs/code/environments/production/modules /home/ec2-user/HelloAfterpay/config/site.pp
