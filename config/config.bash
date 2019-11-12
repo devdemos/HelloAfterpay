3c7fb748ae468789edaab9297921dcddf99956e6
James Skliros <james.skliros@auspost.com.au>
2019-11-13 08:56:31 +1100

    correct location of site.pp

--- config/config.bash
+++ config/config.bash
@@ -2,8 +2,9 @@
 
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
-echo "fs.file-max=565535" >>  /etc/sysctl.conf
+echo "fs.file-max=65535" >>  /etc/sysctl.conf
 sysctl -p
 
 export PATH=/opt/puppetlabs/bin:$PATH
-puppet apply --modulepath=/etc/puppetlabs/code/environments/production/modules /home/ec2-user/HelloAfterpay/config/site.pp
+mv /home/ec2-user/HelloAfterpay/config/site.pp /etc/puppetlabs/code/environments/production/manifests/
+puppet apply --modulepath=/etc/puppetlabs/code/environments/production/modules /etc/puppetlabs/code/environments/production/manifests/site.pp
